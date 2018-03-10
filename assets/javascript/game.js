625a25b3c06244456511c7d263b4876266dc9237
cjohnston32 <clayjohnston@protonmail.com>
2018-03-10 09:58:11 -0500

    Commit 2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,53 @@
-// Version 1.0
+// Version 1.0$
+var score = 0;
+var crystal = [Math.floor((Math.random() * 12) + 1), Math.floor((Math.random() * 12) + 1), Math.floor((Math.random() * 12) + 1), Math.floor((Math.random() * 12) + 1)];  // Random value array from 1-12 for crystals
+var goal = Math.floor((Math.random() * 100) + 20);
+function clicker(num){
+  if(num===1) {
+    score = score + crystal[0];
+    document.getElementById('score').innerHTML = score;
+    console.log(score);
+  }
+  if(num===2) {
+    score = score + crystal[1];
+    document.getElementById('score').innerHTML = score;
+    console.log(score);
+  }
+  if(num===3) {
+    score = score + crystal[2];
+    document.getElementById('score').innerHTML = score;
+    console.log(score);
+  }
+  if(num===4) {
+    score = score + crystal[3];
+    document.getElementById('score').innerHTML = score;
+  }
+}
+var wins = 0;
+var losses = 0;
+if(score>goal) {  // Loss!
+  losses++;
+  document.getElementById('lossesS').innerHTML = losses;
+  score = 0;
+}
+else if(score===goal) {  // Win!
+  wins++;
+  document.getElementById('winsS').innerHTML = wins;
+  score = 0;
+}
 window.onload = function() {
-  var score = 0;
-  var wins = 0;
-  var losses = 0;
-  var goal = Math.floor((Math.random() * 120) + 19);  // Random value from 19-120 for goal
-  var crystal = [Math.floor((Math.random() * 12) + 1), Math.floor((Math.random() * 12) + 1), Math.floor((Math.random() * 12) + 1), Math.floor((Math.random() * 12) + 1)];  // Random value array from 1-12 for crystals
-  document.getElementById('goal').innerHTML = goal;
-  // Every round the player will either 1) Win, 2) Lose, or 3) Continue
-    document.getElementById('c0').onclick = function() {
-      score = score + crystal[0];
-      document.getElementById('score').innerHTML = score;
-    };
-    document.getElementById('c1').onclick = function() {
-      score = score + crystal[1];
-      document.getElementById('score').innerHTML = score;
-    };
-    document.getElementById('c2').onclick = function() {
-      score = score + crystal[2];
-      document.getElementById('score').innerHTML = score;
-    };
-    document.getElementById('c3').onclick = function() {
-      score = score + crystal[3];
-      document.getElementById('score').innerHTML = score;
-    };
-
-    score = document.getElementById('score').innerHTML;
-    if(score>goal) {  // Loss!
-      losses++;
-      document.getElementById('losses').innerHTML = losses;
-      score = 0;
-    }
-    else if(score===goal) {  // Win!
-      wins++;
-      document.getElementById('wins').innerHTML = wins;
-      score = 0;
-    }
+var num = 0;
+var wins = 0;
+var losses = 0;
+document.getElementById('goal').innerHTML = goal;
+}
+if(score>goal) {  // Loss!
+  losses++;
+  document.getElementById('lossesS').innerHTML = losses;
+  score = 0;
+}
+else if(score===goal) {  // Win!
+  wins++;
+  document.getElementById('winsS').innerHTML = wins;
+  score = 0;
 }
